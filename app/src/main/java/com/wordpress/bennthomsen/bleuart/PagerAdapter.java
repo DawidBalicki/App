99b69f7827fcaabac196e92b70b3f36da938dc76
DawidBalicki <dawidbalicki95@gmail.com>
2017-12-12 00:54:37 +0100

    "Added PagerAdapter"

---
+++ app/src/main/java/com/wordpress/bennthomsen/bleuart/PagerAdapter.java
@@ -0,0 +1,38 @@
+package com.wordpress.bennthomsen.bleuart;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNoOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumberOfTabs) {
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position)
+        {
+
+            case 0:
+                return new AdminDefaultTab();
+            case 1:
+                return new AdminCreateAccountTab();
+            case 2:
+                return new AdminShowUsersTab();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNoOfTabs;
+    }
+}
